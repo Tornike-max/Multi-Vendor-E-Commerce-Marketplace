ac10b3a6abaf2226b0049088fbd63207b6b06bac
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-12-27 14:00:50 +0400

    i changed little things

--- root/database/migrations/2024_12_24_204654_create_categories_table.php
+++ root/database/migrations/2024_12_24_204654_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('department_id')->index()->constrained();;
+            $table->foreignId('department_id')->index()->constrained();
             $table->foreignId('parent_id')->nullable()->index()->constrained('categories');
             $table->boolean('active')->default(true);
             $table->timestamps();
