b06c745e2c7386ae6396a5cda61c18c2f3cbbc79
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-12-31 02:39:59 +0400

    i created productResource

---
+++ root/app/Filament/Resources/ProductResource.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\Enums\ProductStatusEnum;
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+use Filament\Forms\Components\RichEditor;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make()->schema([
+                    TextInput::make('title')->live(onBlur: true)->afterStateUpdated(function (string $operation, $state, callable $set) {
+                        $set('slug', Str::slug($state));
+                    }),
+                    TextInput::make('slug')->required(),
+                    Select::make('department_id')
+                        ->relationship('department', 'name')
+                        ->label(__('Department'))
+                        ->preload()
+                        ->searchable()
+                        ->required()
+                        ->reactive()
+                        ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                            $set('category_id', null);
+                        }),
+                    Select::make('category_id')
+                        ->relationship(name: 'category', titleAttribute: 'name', modifyQueryUsing: function (Builder $query, callable $get) {
+                            $departmentId = $get('department_id');
+                            if ($departmentId) {
+                                $query->where('department_id', $departmentId);
+                            }
+                        })->label(__('Category'))->required()->preload()->searchable()->reactive()
+                ]),
+                RichEditor::make('description')->required()->toolbarButtons([
+                    'blockquote',
+                    'bold',
+                    'italic',
+                    'bulletList',
+                    'h2',
+                    'h3',
+                    'link',
+                    'orderedList',
+                    'redo',
+                    'strike',
+                    'underline',
+                    'undo',
+                    'table'
+                ])->columnSpan(2),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('quantity')
+                    ->required()
+                    ->numeric(),
+                Select::make('status')
+                    ->options(ProductStatusEnum::labels())
+                    ->default(ProductStatusEnum::Draft->value)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
--- root/app/Models/Product.php
+++ root/app/Models/Product.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
